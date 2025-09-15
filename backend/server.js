a569f969b15ba9decdd5bc0d836fa0520bba279a
KeithYao1012 <147405253+KeithYao1012@users.noreply.github.com>
2025-09-14 21:49:53 -0700

    Added models

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config({ path: './backend/.env'});
 connectDB();
@@ -10,6 +11,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/users", userRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`🚀 Server running on port ${process.env.PORT || 5000}`);
