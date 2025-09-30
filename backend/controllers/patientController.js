08bc9e19922a02e4c1b95db49bb3e1b5e880e801
KeithYao1012 <147405253+KeithYao1012@users.noreply.github.com>
2025-09-30 07:46:11 -0700

    Changed patient controller to create User object first

--- backend/controllers/patientController.js
+++ backend/controllers/patientController.js
@@ -1,13 +1,39 @@
 // controllers/patientController.js
 import Patient from "../models/Patient.js";
-
+import User from "../models/User.js";
 // Create new patient
 export const createPatient = async (req, res) => {
   try {
-    console.log("Creating Patient")
-    const patient = new Patient(req.body);
+    const userData = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      username: req.body.username,
+      gender: req.body.gender,
+      password: req.body.password, // Hash before saving in production
+      phoneNumber: req.body.phoneNumber,
+      profilePic: req.body.profilePic,
+      role: "Patient",
+    };
+    const user = new User(userData);
+    const savedUser = await user.save();
+  
+    const patientData = {
+      user: savedUser._id, // link Patient → User
+      birthday: req.body.birthday,
+      address: req.body.address,
+      bloodtype: req.body.bloodtype,
+      allergies: req.body.allergies,
+      medicalHistory: req.body.medicalHistory,
+      emergencyContact: req.body.emergencyContact,
+    };
+
+    const patient = new Patient(patientData);
     const savedPatient = await patient.save();
-    return res.status(201).json(savedPatient);
+    return res.status(201).json({
+      user: savedUser,
+      patient: savedPatient,
+    });
   } catch (err) {
     return res.status(400).json({ error: err.message });
   }
@@ -29,7 +55,7 @@ export const getAllPatients = async (req, res) => {
 // Get single patient by ID
 export const getPatientById = async (req, res) => {
   try {
-    const patient = await Patient.findById(req.params.id).populate(
+    const patient = await Patient.findOne({ user: req.params.userId }).populate(
       "user",
       "firstName lastName email username gender phoneNumber profilePic role"
     );
@@ -43,8 +69,8 @@ export const getPatientById = async (req, res) => {
 // Update patient by ID
 export const updatePatient = async (req, res) => {
   try {
-    const updatedPatient = await Patient.findByIdAndUpdate(
-      req.params.id,
+    const updatedPatient = await Patient.findOneAndUpdate(
+      { user: req.params.userId },
       req.body,
       { new: true, runValidators: true }
     );
@@ -58,7 +84,8 @@ export const updatePatient = async (req, res) => {
 // Delete patient by ID
 export const deletePatient = async (req, res) => {
   try {
-    const deletedPatient = await Patient.findByIdAndDelete(req.params.id);
+    const deletedPatient = await Patient.findOneAndDelete({ user: req.params.userId });
+
     if (!deletedPatient) return res.status(404).json({ error: "Patient not found" });
     return res.json({ message: "Patient deleted successfully" });
   } catch (err) {
