8f16d6a28c02495c3e15dae5b609e552a97f6008
Gayatri <kgayatri2005@gmail.com>
2025-10-11 13:59:50 -0700

    patient routes oops

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -1,5 +1,9 @@
 // routes/patientRoutes.js
 import express from "express";
+<<<<<<< HEAD
+=======
+import { protect } from "../middleware/authentication.js";
+>>>>>>> 01638b4 (patient routes oops)
 import {
   createPatient,
   getAllPatients,
@@ -11,9 +15,16 @@ import {
 const router = express.Router();
 
 router.post("/", createPatient);
+<<<<<<< HEAD
 router.get("/", getAllPatients);
 router.get("/:id", getPatientById);
 router.put("/:id", updatePatient);
 router.delete("/:id", deletePatient);
+=======
+router.get("/", protect, getAllPatients);
+router.get("/:id", protect, getPatientById);
+router.put("/:id", protect, updatePatient);
+router.delete("/:id", protect, deletePatient);
+>>>>>>> 01638b4 (patient routes oops)
 
 export default router;
