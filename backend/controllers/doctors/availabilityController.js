74e0eff852aa81e380e38ae360feed703b39830b
KeithYao1012 <147405253+KeithYao1012@users.noreply.github.com>
2025-10-26 15:46:42 -0400

    Changed availabilityController import

--- backend/controllers/doctors/availabilityController.js
+++ backend/controllers/doctors/availabilityController.js
@@ -1,5 +1,5 @@
 import Availability from "../../models/doctors/Availability.js";
-import Doctor from "../models/Doctor.js";
+import Doctor from "../../models/doctors/Doctor.js";
 
 export const createRecurringAvailability = async (req, res) => {
   try {
