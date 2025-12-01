b6a2248dea471d72a639413646403ee30f02dbf2
KeithYao1012 <147405253+KeithYao1012@users.noreply.github.com>
2025-12-01 15:41:28 -0800

    test

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
-
 // Setup __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,7 +12,6 @@ if (!process.env.JWT_SECRET || !process.env.MONGO_URI) {
   console.error("❌ Missing JWT_SECRET or MONGO_URI in .env file");
   process.exit(1);
 }
-
 
 console.log("✅ Environment variables loaded");
 
@@ -35,7 +33,8 @@ import patientRequestChangeRoutes from "./routes/tickets/patientRequestChangeRou
 import doctorRequestChangeRoutes from "./routes/tickets/doctorRequestChangeRoutes.js";
 import bugTicketRoutes from "./routes/tickets/bugTicketRoutes.js";
 import availabilityRoutes from "./routes/doctors/availabilityRoutes.js";
-import authRoutes from "./routes/auth/authRoutes.js";import chatRoutes from "./routes/chat/chatRoutes.js";
+import authRoutes from "./routes/auth/authRoutes.js";
+import chatRoutes from "./routes/chat/chatRoutes.js";
 // Setup Express
 const app = express();
 
